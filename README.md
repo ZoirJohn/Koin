256b972fa854f3ec3a1b66fa1b41744d1492a9f3
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2022-08-09 20:18:19 +0500

    finished the work with header

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Koin
+
+#### Header is ready on August,9
+
+> In this header I use:
+	* My teacher's develompent 'Dynamic-Adaptive'
+	* Own scripts
+
+In <main></main> I can use a lot of <em>JavaScript</em> code 
---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+const burger = document.querySelector('.burger');
+const body = document.querySelector('body');
+const menu = document.querySelector('.menu');
+burger.addEventListener('click', function () {
+   burger.classList.toggle('cross_');
+   body.classList.toggle('lock_');
+   menu.classList.toggle('open_');
+});
