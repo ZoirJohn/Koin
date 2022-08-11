e42be55868bb324db16b3956d766e74a4175db2f
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2022-08-11 16:54:16 +0500

    created one section in <main></main>

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const burger = document.querySelector('.burger');
 const body = document.querySelector('body');
 const menu = document.querySelector('.menu');
 burger.addEventListener('click', function () {
-   burger.classList.toggle('cross_');
-   body.classList.toggle('lock_');
-   menu.classList.toggle('open_');
+   if (burger && body && menu) {
+      burger.classList.toggle('cross_');
+      body.classList.toggle('lock_');
+      menu.classList.toggle('open_');
+   }
 });
